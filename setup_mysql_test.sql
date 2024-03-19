9e58a07b1d8ab2f42f57805334b60966a5255ecd
abeni-al7 <abenezeralebachew3@gmail.com>
2024-03-19 20:57:04 +0300

    Correct a small syntax error near the comments of setup_mysql_test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,5 @@
---Sql Script to setup test db
---creates a database a new user with specified permissions
+-- Sql Script to setup test db
+-- creates a database a new user with specified permissions
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 FLUSH PRIVILEGES;
