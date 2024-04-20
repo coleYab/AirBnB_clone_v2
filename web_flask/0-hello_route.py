609a8046f46b4d52616345b291feb9b30c3d2f19
coleYab <yabume123@gamil.com>
2024-04-20 21:39:43 +0300

    added files

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+""" Runs file to the name """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ runs hbnb at the root """
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0:5000')
