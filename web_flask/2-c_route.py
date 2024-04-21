c7e424c8b1f8cc04a0d2221927c070ccebdc47d6
coleYab <yabume123@gamil.com>
2024-04-21 08:34:38 +0300

    add 2-c_route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+0-hello route: module to do the text.
+runs: hbnb
+"""
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    hello_hbnb: runs hbnb at the root
+    args: none
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """
+    hello_hbnb: runs hbnb at the root
+    args: none
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hello_hbnb(text: str):
+    """
+    hello_hbnb: runs hbnb at the root
+    args: none
+    """
+    return f'c {escape(text).replace('_', ' ')}'
+
+
+if __name__ == '__main__':
+    """
+    runs the applications
+    """
+    app.run(host='0.0.0.0', port=5000)
