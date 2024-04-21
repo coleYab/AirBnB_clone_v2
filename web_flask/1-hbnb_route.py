f3d78d35e05eeb9b5b2b36ed6b7d1074631a4fde
coleYab <yabume123@gamil.com>
2024-04-21 08:26:36 +0300

    added 1 hbnb

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+0-hello route: module to do the text.
+runs: hbnb
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    hello_hbnb: runs hbnb at the root
+    args: none
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """
+    hello_hbnb: runs hbnb at the root
+    args: none
+    """
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    """
+    runs the applications
+    """
+    app.run(host='0.0.0.0', port=5000)
