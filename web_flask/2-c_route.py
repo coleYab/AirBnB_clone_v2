b60b4db2ae4c48e7b6cd7822461dee2812aa1d79
coleYab <yabume123@gamil.com>
2024-04-21 08:44:24 +0300

    add 2 - c route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -32,7 +32,7 @@ def c_is(text: str):
     hello_hbnb: runs hbnb at the root
     args: none
     """
-    return f'c {escape(text).replace('_', ' ')}'
+    return f'c {escape(text.replace('_', ' '))}'
 
 
 if __name__ == '__main__':
